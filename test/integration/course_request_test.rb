df6c759d4faeeb0bca0aac54c4f6f67861dca722
Wolfgang Vogl <wuulf01@yahoo.de>
2012-02-10 12:55:03 +0100

    update course_request integration test

--- test/integration/course_request_test.rb
+++ test/integration/course_request_test.rb
@@ -15,4 +15,41 @@ class CourseTest < ActionDispatch::IntegrationTest
     assert page.has_content?(course_request2.description)
   end
 
+  should 'show course_request show action' do
+    course_request = Factory.create(:course_request)
+    visit "/"
+    click_on I18n.t('app.course_request_link')
+    click_on course_request.title.to_s
+    assert page.has_content?(course_request.title)
+    assert page.has_content?(course_request.description)
+  end
+
+  should 'show course_request new action' do
+    login_as
+    visit "/course_requests/new"
+    assert page.has_content?(I18n.t('course_request.new.headline'))
+    assert page.has_content?(I18n.t('course_request.new.title'))
+    assert page.has_content?(I18n.t('course_request.new.description'))
+  end
+
+  should 'be able to create course_request' do
+    login_as
+    click_on I18n.t('app.course_request_link')
+    click_on "create_course_request_button"
+    assert_difference "CourseRequest.count" do
+      fill_in('course_request_title', :with => 'Java programmierung')
+      fill_in('course_request_description', :with => 'Applikationsprogrammierung mit Java')
+      click_on I18n.t('course_request.new.submit')
+      save_and_open_page
+    end
+    assert current_path.to_s == course_requests_path
+    assert page.has_content?(I18n.t('course_request.create.success'))
+
+  end
+
+  should 'not be able to create course_request without login' do
+    visit "/course_requests/new"
+    assert page.has_content?(I18n.t('devise.sessions.title'))
+    assert page.has_content?(I18n.t('devise.failure.unauthenticated'))
+  end
 end
