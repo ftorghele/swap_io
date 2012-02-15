ebf21e3273d0a2c5891346f18a0f3c3228db2bb1
Wolfgang Vogl <wuulf01@yahoo.de>
2012-02-15 11:39:02 +0100

    bugfix in behaviour of attend_course

--- test/integration/attend_course_test.rb
+++ test/integration/attend_course_test.rb
@@ -17,10 +17,10 @@ class AttendCourseTest < ActionDispatch::IntegrationTest
   should 'not be able to attend own course' do
     user = Factory.create(:user)
     course = Factory.create(:course, :user => user)
-    login_as user
+    login_as(user)
     visit "/"
     click_on course.title
-    assert page.has_no_content?(I18n.t('course.show.attend_course_button'))
+    assert page.has_no_button?(I18n.t('course.show.attend_course_button'))
   end
 
   should 'not be able to attend course without login' do
@@ -28,7 +28,25 @@ class AttendCourseTest < ActionDispatch::IntegrationTest
     course = Factory.create(:course, :user => user)
     visit "/"
     click_on course.title
-    assert page.has_no_content?(I18n.t('course.show.attend_course_button'))
+    assert page.has_no_button?(I18n.t('course.show.attend_course_button'))
   end
 
+  should 'be able to attend more than one course' do
+    user1 = Factory.create(:user)
+    course1 = Factory.create(:course, :user => user1)
+    course2 = Factory.create(:course, :user => user1)
+    login_as
+    visit "/"
+    click_on course1.title
+    click_on(I18n.t('course.show.attend_course_button'))
+    assert page.has_content?(I18n.t('attend_course.create.success'))
+    click_on course1.title
+    assert page.has_no_button?(I18n.t('course.show.attend_course_button'))
+    visit "/"
+    click_on course2.title
+    click_on(I18n.t('course.show.attend_course_button'))
+    assert page.has_content?(I18n.t('attend_course.create.success'))
+    click_on course2.title
+    assert page.has_no_button?(I18n.t('course.show.attend_course_button'))
+  end
 end
