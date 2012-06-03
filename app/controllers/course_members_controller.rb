480bf32f2bca75d9fce966a8c6ec75647f0efa9d
Wolfgang Vogl <wuulf01@yahoo.de>
2012-06-04 00:33:42 +0200

    add accept/reject buttons to manage view

--- app/controllers/course_members_controller.rb
+++ app/controllers/course_members_controller.rb
@@ -13,14 +13,15 @@ class CourseMembersController < ApplicationController
   end
 
   def update
-    if course_member = CourseMember.update_user_acceptance(params[:id], params[:acceptance])
+    @course_member = CourseMember.find_by_id(params[:id])
+    if cm = CourseMember.update_user_acceptance(params[:id], params[:acceptance])
       flash[:info] = I18n.t('course_member.update.success')
-    elsif course_member == false
+    elsif cm == false
       flash[:info] = I18n.t('course_member.update.rejected')
     else
       flash[:error] = I18n.t('course_member.update.fail')
     end
-    redirect_to root_path
+    redirect_to "#{manage_course_path(@course_member.course.id)}/#{@course_member.id}/"
   end
 
   def destroy
@@ -36,7 +37,6 @@ class CourseMembersController < ApplicationController
 
   def get_course
     unless @course = Course.find_by_id(params[:course_id])
-      puts @course.inspect
       flash[:error] = I18n.t('msg.not_found')
       redirect_to courses_path and return
     end
