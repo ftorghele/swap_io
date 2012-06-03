b19325197efb2c7db6863825a2e223ecc8772ccb
Wolfgang Vogl <wuulf01@yahoo.de>
2012-06-03 21:32:03 +0200

    acceptance helper added

--- app/helpers/course_members_helper.rb
+++ app/helpers/course_members_helper.rb
@@ -1,2 +1,13 @@
 module CourseMembersHelper
+
+  def course_member_acceptance
+    case @course_member.accepted
+    when 2
+      '<p id="acceptance-pending">Anfrage versendet</p>'
+    when 1
+      '<p id="acceptance-true">Anfrage angenommen</p>'
+    else
+      '<p id="acceptance-false">Anfrage abgelehnt</p>'
+    end
+  end
 end
