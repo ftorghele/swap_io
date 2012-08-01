db64663723a1018656856ee501560e96f4ba7c65
Wolfgang Vogl <wuulf01@yahoo.de>
2012-08-02 01:54:49 +0200

    add delete button styled newsletter

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,3 @@
-every 1.day, :at => '1:25 am' do
-  runner "CourseMember.course_member_notification_task"
+every 1.day, :at => '2:00 am' do
+  runner "CourseMember.course_member_notification_task", :output => {:error => 'error.log', :standard => 'cron.log'}
 end
