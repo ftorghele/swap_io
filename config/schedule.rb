67a0c698fb028488056dde3d6879b166f543c29e
Wolfgang Vogl <wuulf01@yahoo.de>
2012-08-03 16:48:44 +0200

    update

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,6 +2,6 @@ every 1.day, :at => '1:25 am' do
   runner "CourseMember.course_member_notification_task", :output => {:error => 'error.log', :standard => 'cron.log'}
 end
 
-every 1.day, :at => '4:44 pm' do
+every :hour do
   command "echo 'testing' >> testfile.txt", :output => {:error => 'error.log', :standard => 'cron.log'}
 end
