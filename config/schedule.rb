70459ebc6fed4ea86e853ce21dae6af1cf97a0a8
Wolfgang Vogl <wuulf01@yahoo.de>
2012-08-06 18:22:43 +0200

    update cronjob file

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,8 +1,4 @@
-every 1.day, :at => '1:25 am' do
-  runner "CourseMember.course_member_notification_task", :output => {:error => 'error.log', :standard => 'cron.log'}
-end
-
-every :hour do
+every 1.day, :at => '00:05 am' do
   command "echo 'das ist ein test' >> /home/deployer/apps/swap_io/current/cron_test.txt"
-
+  runner "CourseMember.course_member_notification_task", :output => {:error => 'error.log', :standard => 'cron.log'}
 end
